1d7c7e07c039dd7db0745ed3b36cbddf62f67606
Félix Pardo rivera <fpardo@poligran.edu.co>
2022-12-04 16:43:26 -0500

    error 7

--- Api.User/Controllers/UserController.cs
+++ Api.User/Controllers/UserController.cs
@@ -60,7 +60,7 @@ namespace User.Controllers
                 int Ire = await _context.SaveChangesAsync();
                 if (Ire > 0)
                 {
-                    svRta = new string[] { "Guardo felix correctamente." };
+                    svRta = new string[] { "Guardo felix2 correctamente." };
                 }
                 else
                 {
--- Jenkinsfile
+++ Jenkinsfile
@@ -4,7 +4,6 @@ pipeline{
          stage("Build docker image"){
            steps{
              script {
-                bat 'docker compose -f "docker-compose.yml" down '
                 bat 'docker compose  -f "docker-compose.yml" up -d --build sqldata user '
              }
            }
