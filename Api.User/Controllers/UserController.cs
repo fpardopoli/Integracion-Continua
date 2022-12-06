25c29bd26e3515a01824c9a68c7c72b8d6b67e65
Félix Pardo rivera <fpardo@poligran.edu.co>
2022-12-05 20:56:36 -0500

    delete pedito

--- Api.User/Controllers/UserController.cs
+++ Api.User/Controllers/UserController.cs
@@ -60,7 +60,7 @@ namespace User.Controllers
                 int Ire = await _context.SaveChangesAsync();
                 if (Ire > 0)
                 {
-                    svRta = new string[] { "Guardo Pepito correctamente." };
+                    svRta = new string[] { "Guardo correctamente." };
                 }
                 else
                 {
